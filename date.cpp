e7e21db616ea1b89fd0430f4926c8fed662f3e3d
smart--petea <smart__petea@mail.ru>
2015-04-22 20:33:33 +0300

    1.4.7

--- date.cpp
+++ date.cpp
@@ -42,3 +42,40 @@ void print_in_words(Date& date, ostream& out)
 {
     out << Date::months[date.month] << " " << date.day + 1 << ", " << date.year;
 }
+
+void add(Date& date, int num_days)
+{
+    int total = date.day + num_days;
+    if(total < 0) 
+    {
+        date.day = total % 30 + 30;
+        total -= 30;
+    } else {
+        date.day = total % 30;
+    }
+
+    total = total / 30 + date.month;
+    if(total < 0)
+    {
+        date.month = total % 12 + 12;
+        total -= 12;
+    } else {
+        date.month = total % 12;
+    }
+
+    date.year += total / 12;
+    if(date.year < 0) date.year = 0;
+}
+
+int main()
+{
+    Date d;
+    initialize(d);
+    print(d, cout);
+    cout << endl;
+
+    add(d, -1);
+    print_in_words(d, cout);
+
+    return 0;
+}
