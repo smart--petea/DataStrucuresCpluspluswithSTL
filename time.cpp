97fa11c23e76bd3d3e9e07614974b07a50086212
smart--petea <smart__petea@mail.ru>
2015-04-23 12:42:57 +0300

    ch2 time.cpp

--- time.cpp
+++ time.cpp
@@ -1,81 +1,38 @@
 #include <sstream>
-#include <iostream>
-#include <assert.h>
-
 using namespace std;
 
-#ifndef TIME
-#define TIME
-struct Time
+class Time
 {
-    int hours;
-    int minutes;
+    friend void initialize(Time & t);
+    friend void read(Time& t, istream& in);
+    friend void print(const Time& t, ostream& out);
+    friend double difference(const Time& t1, const Time& t2);
+
+    private:
+        int hours;
+        int minutes;
 };
 
-void normalize(Time& t)
+void initialize(Time& t)
 {
-    //hours in [0, 23]
-    //minutes in [0, 59]
-    t.hours += t.minutes < 0 ? (t.minutes - 60)/ 60 : t.minutes / 60;
-    t.hours = (t.hours < 0) ? t.hours % 24 + 24 : t.hours % 24;
-    t.minutes = (t.minutes < 0) ? t.minutes % 60 + 60 : t.minutes % 60; 
-}
-
-void add_minutes(Time& t, int num_minutes)
-{
-    t.minutes += num_minutes;
-    normalize(t);
-}
-
-void initialize(Time& t, int hours = 23, int minutes = 59) //initialize(t, 100, 100)
-{
-    t.hours = hours;
-    t.minutes = minutes;
-    normalize(t);
+    t.hours = t.minutes = 99;
 }
 
 void read(Time& t, istream& in)
 {
-    in >> t.hours;  //8
-    in.get();       //:
-    in >> t.minutes;//35
-    in.get();       //' '
-
-    char c;
-    in.get(c);      //a
-
-    if(c == 'p') t.hours += 12;
-
-    in.get();       //.
-    in.get();       //m
-    in.get();       //.
+    in >> t.hours;
+    in.get(); //colon
+    in >> t.minutes;
 }
 
 void print(const Time& t, ostream& out)
 {
-    char c = 'a';
-    int hours = t.hours;
-
-    if(hours >= 12)
-    {
-        hours -= 12;
-        c = 'p';
-    }
-
-    out << hours << ':';
+    out << t.hours << ':';
     if(t.minutes < 10) out << 0;
     out << t.minutes;
-    out << " " << c << ".m.";
 }
 
 double difference(const Time& t1, const Time& t2)
 {
     return (t1.hours + t1.minutes/60.0) - (t2.hours + t2.minutes/60.0);
 }
-
-bool is_later_than(const Time& t1, const Time& t2)
-{
-    return (t1.hours > t2.hours) || (t1.hours == t2.hours && t1.minutes > t2.minutes);
-}
-
-#endif
